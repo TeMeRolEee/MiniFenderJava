d8960f55fb2ccc572cd13d3513ba8454a02fe2e0
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-03-15 20:01:06 +0100

    Started working on WorkerThread

--- src/com/company/WorkerThread.java
+++ src/com/company/WorkerThread.java
@@ -1,4 +1,24 @@
 package com.company;
 
-public class WorkerThread {
+
+import java.util.List;
+import java.util.UUID;
+
+public class WorkerThread extends Thread {
+
+    private final UUID id;
+    private final String enginePath;
+    private final List<String> paramList;
+
+    public WorkerThread(UUID id, String enginePath, List<String> paramList, List<String> paramList1) {
+        this.enginePath = enginePath;
+        this.id = id;
+        this.paramList = paramList1;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
 }
