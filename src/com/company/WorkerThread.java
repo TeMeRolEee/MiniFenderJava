1f3d729647e0ef28fa77bd3f3787f8f35b70df3e
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-03-15 20:44:03 +0100

    Processbuilder added

--- src/com/company/WorkerThread.java
+++ src/com/company/WorkerThread.java
@@ -1,6 +1,8 @@
 package com.company;
 
 
+import com.github.msteinbeck.sig4j.signal.Signal1;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -9,16 +11,20 @@ public class WorkerThread extends Thread {
     private final UUID id;
     private final String enginePath;
     private final List<String> paramList;
+    final ProcessBuilder process;
 
-    public WorkerThread(UUID id, String enginePath, List<String> paramList, List<String> paramList1) {
+    private final Signal1<String> signal1 = new Signal1<>();
+
+    public WorkerThread(UUID id, String enginePath, List<String> paramList) {
         this.enginePath = enginePath;
         this.id = id;
-        this.paramList = paramList1;
+        this.paramList = paramList;
+
+        process = new ProcessBuilder(enginePath.toString(),paramList.get(0).toString(),paramList.get(1).toString());
+        this.signal1.connect(this::processDone_slot);
     }
 
-    @Override
-    public void run() {
-        super.run();
-    }
+    private void processDone_slot(String s) {
 
+    }
 }
