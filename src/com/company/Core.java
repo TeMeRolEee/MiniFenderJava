a94325a3e2ed652384a10017a14758ccf4e9ad92
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-03-15 18:18:46 +0100

    Added classes

---
+++ src/com/company/Core.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Core extends Thread {
+    boolean init() {
+
+        return false;
+    }
+}
---
+++ src/com/company/DBManager.java
@@ -0,0 +1,4 @@
+package com.company;
+
+public class DBManager extends Thread {
+}
