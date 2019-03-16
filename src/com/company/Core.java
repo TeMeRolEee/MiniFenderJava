44543a1ad4bd1754ea191ed1cb3d1676e4a0a47e
Réthalmi Dávid <david.rethalmi@gmail.com>
2019-03-16 17:44:22 +0100

    Modified init and readSettings

--- src/com/company/Core.java
+++ src/com/company/Core.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.github.msteinbeck.sig4j.signal.Signal1;
 import netscape.javascript.JSObject;
 import org.ini4j.Ini;
 import org.json.simple.JSONObject;
@@ -7,11 +8,14 @@ import org.json.simple.JSONObject;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class Core extends Thread {
+    private Signal1<List<String>> addNewEngine_signal;
+    private Signal1<Map<UUID, String>> startNewScanTask_signal;
+    private Signal1<Integer> removeEngines_signal;
+    private Signal1<UUID> startCalculateResult_signal;
+
     private String rootDirectory;
     EngineHandler engineHandler;
     DBManager dbManager;
@@ -19,13 +23,34 @@ public class Core extends Thread {
 
     Map<UUID, JSObject> scanMap;
 
-    private JSONObject calculateResult(UUID uuid) {
+    private JSONObject calculateResult_slot(UUID uuid) {
         return null;
     }
 
 
     boolean init(String rootDirectory) {
-        this.rootDirectory = rootDirectory;
+        if (!rootDirectory.isEmpty()) {
+            this.rootDirectory = rootDirectory;
+
+            addNewEngine_signal = new Signal1<>();
+            engineHandler = new EngineHandler();
+            dbManager = new DBManager();
+            cliHandler = new CliHandler();
+            startNewScanTask_signal = new Signal1<>();
+            removeEngines_signal = new Signal1<>();
+            startCalculateResult_signal = new Signal1<>();
+
+            startCalculateResult_signal.connect(this::calculateResult_slot);
+            //startNewScanTask_signal.connect();
+
+            if (!dbManager.init(rootDirectory + "\\db\\scanHistoryDB.sqlite")) {
+                return false;
+            }
+
+            
+
+            return true;
+        }
 
         return false;
     }
@@ -39,5 +64,13 @@ public class Core extends Thread {
         }
         Set<String> strings = ini.keySet();
 
+        for (int i = 0; i < strings.size(); i++) {
+            String key = (String) strings.toArray()[i];
+            List<String> childrenNames = Arrays.asList(ini.get(key).childrenNames());
+            if (childrenNames.contains("path") && childrenNames.contains("scan_parameter")) {
+
+            }
+
+        }
     }
 }
